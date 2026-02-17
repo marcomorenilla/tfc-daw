e4361cbfb9626b11df02512a75ffe76c525f741d
marco <marcomorenilla@gmail.com>
2026-02-17 21:16:51 +0100

    configuro relaciones entre primeros servicios con uv

---
+++ micro-users/src/micro_users/main.py
@@ -0,0 +1,11 @@
+from shared_utils.main import say_hi
+
+
+def main():
+    print("Hello from micro-users!")
+    say_hi()
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ shared-utils/README.md
@@ -0,0 +1,6 @@
+# Servicio auxiliar de JWT
+
+Aquí se van a producir las operaciones:
+- Generar/Firmar token
+- Validar/Verificar token
+- Decodificar token

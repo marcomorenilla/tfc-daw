0e138422c4e345ce0a9120c3af6b0256184519cd
marco <marcomorenilla@gmail.com>
2026-03-18 18:17:26 +0100

    crud de usuarios

--- backend/micro-users/src/micro_users/db/session.py
+++ backend/micro-users/src/micro_users/db/session.py
@@ -9,7 +9,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     db = SessionLocal()
--- backend/micro-users/src/micro_users/services/__init__.py
+++ backend/micro-users/src/micro_users/services/__init__.py
@@ -1,1 +1,7 @@
-from .users_service import get_user_by_email, get_all_users, authenticate_user, create_user
+from .users_service import (
+    get_user_by_email,
+    get_all_users,
+    authenticate_user,
+    create_user,
+    update_user,
+)

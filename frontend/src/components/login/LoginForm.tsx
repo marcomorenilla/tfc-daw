2ac21b3f37753f40106d98c933e7aa238a6114f5
marco <marcomorenilla@gmail.com>
2026-03-22 18:55:20 +0100

    Cambio diseño de login

--- frontend/src/components/login/LoginForm.tsx
+++ frontend/src/components/login/LoginForm.tsx
@@ -34,46 +34,60 @@ export default function LoginForm({ apiUrl }: LoginFormProps) {
     }
   };
   return (
-    <form
-      className="w-auto mt-5 flex flex-col gap-4 items-center justify-center"
-      onSubmit={handleSubmit}
-      id="login-form">
-      <section className="relative flex flex-col gap-3">
-        <label htmlFor="username" className="font-bold text-lg">
-          Username:
-        </label>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          required
-          className="border rounded-sm p-1 w-auto border-(--color-primary) outline-none focus:ring-4 focus:ring-(--color-primary)/50"
-        />
-      </section>
-      <section className="relative flex flex-col gap-3">
-        <label htmlFor="password" className="font-bold text-lg">
-          Password:
-        </label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          required
-          className="border rounded-sm p-1 w-auto border-(--color-primary) outline-none focus:ring-4 focus:ring-(--color-primary)/50"
-        />
-      </section>
-      <section className="flex gap-2">
-        <button
-          className="cursor-pointer p-2 text-white rounded-sm font-semibold  bg-(--color-primary) hover:border hover:border-(--color-primary) hover:text-(--color-primary)  hover:bg-(--color-bg) "
-          type="submit">
-          Login
+    <>
+      <form
+        className="w-auto mt-5 border-b-3 border-orange-500/75 pb-5 flex flex-col gap-4 items-center justify-center"
+        onSubmit={handleSubmit}
+        id="login-form">
+        <section className="relative flex flex-col gap-3">
+          <label
+            htmlFor="username"
+            className="font-bold text-lg text-orange-500 text-xl">
+            Username:
+          </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            required
+            className="border rounded-sm p-1 w-auto bg-white/50 border-white/50 outline-none focus:ring-4 focus:ring-(--color-primary)/50"
+          />
+        </section>
+        <section className="relative flex flex-col gap-3">
+          <label
+            htmlFor="password"
+            className="font-bold text-lg text-orange-500 text-xl">
+            Password:
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+            className="border rounded-sm p-1 bg-white/50 w-auto border-white/50 outline-none focus:ring-4 focus:ring-(--color-primary)/50"
+          />
+        </section>
+        <section className=" text-orange-500  flex items-center justify-center font-bold">
+          <a
+            href="/reset-password"
+            className="cursor-pointer md:text-lg text-(--color-primary-dark) hover:border-b hover:border-white hover:text-white">
+            He olvidado mi contraseña
+          </a>
+        </section>
+        <section className="flex gap-2">
+          <button
+            className="cursor-pointer p-2 font-bold md:text-lg text-white rounded-sm   bg-(--color-primary) hover:border hover:border-(--color-primary) hover:text-(--color-primary)  hover:bg-(--color-bg) "
+            type="submit">
+            Entrar
+          </button>
+        </section>
+      </form>
+      <section className="flex md:text-lg flex-col justify-center items-center gap-1 p-1 text-orange-500 font-bold">
+        <p>¿No tienes cuenta?</p>
+        <button className="text-(--color-primary-dark) hover:text-white hover:border-b hover:border-white">
+          Crear cuenta
         </button>
-        <button
-          className="cursor-pointer p-2 text-(--color-primary) rounded-sm font-semibold bg-white border border-(--color-primary) hover:bg-(--color-primary) hover:text-white "
-          type="button">
-          Register
-        </button>
       </section>
-    </form>
+    </>
   );
 }
